51949b40a967dddeaabfcd4388ef7b2df9c36e7a
Lexa67 <3aj4ik504@gmail.com>
2024-05-23 17:38:41 +0300

    Add columns to user

---
+++ db/migrate/20240523123434_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :age, :integer
+    add_column :users, :weight, :integer
+    add_column :users, :gender, :string
+  end
+end
